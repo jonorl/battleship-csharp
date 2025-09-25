4c6bcb9de581171b76d6278c983c8da027cbf902
jonorl <39122230+jonorl@users.noreply.github.com>
2025-09-25 12:08:22 +0300

    Stop tracking auto-generated

--- obj/Debug/net8.0/battleship-csharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/battleship-csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("battleship-csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03743d3b98e1f6cfb2265ad47a76e03baa69f8e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df49a28743e516496ed0e0c9d7163a05d3e93c77")]
 [assembly: System.Reflection.AssemblyProductAttribute("battleship-csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("battleship-csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
